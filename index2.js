36e43da34fe25b38b1f5d821ec849165bfaf9668
user741852 <wo.xiaoyao.zizai@163.com>
2020-08-10 22:00:40 +0800

    the original version

--- index2.js
+++ index2.js
@@ -1,49 +1,16 @@
 const { spawn } = require("child_process")
 const { exec } = require("child_process")
-const huya_danmu = require('../huya-danmu/index')
-const request = require('request-promise')
 
 var express = require('express')
 var app = express()
 
-let isLive = false;
 let output = ""
 var proc = null
+let retryCount = 0;
 let earlyTerminated = false;
 
-const r = request.defaults({ json: true, gzip: true, headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36' } })
-//判断当前开播状态
-async function isLiveOrNot(){
-    let body = await r({
-        url: `https://www.huya.com/lovetuleisi`,
-        agent: this._agent
-    })
-    let roomData = JSON.parse(body.match(/var TT_ROOM_DATA =(.*?);var TT_.{0,18}=/)[1])
-    if(roomData.state === 'ON'){
-        isLive = true;
-        //判断当前是否在录制
-        exec("jps", (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            if((/^.+\n$/.test(stdout))){
-                startRecord("lovetuleisi")
-            }
-        });
-    }else{
-        isLive = false;
-    }
-}
-isLiveOrNot()
-
-//调用java程序进行录制
-function startRecord(roomId) {
-    proc = spawn("java", ["-Dfile.encoding=utf-8", "-jar", "BiliLiveRecorder.jar", "debug=false&check=false&delete=false&liver=huya&id=" + roomId + "&retry=3&qn=-1&qnPri=蓝光4M>超清>高清>流畅"], { cwd: '/home/ubuntu' })
+function startRecord(roomId, retryCount) {
+    proc = spawn("java", ["-Dfile.encoding=utf-8", "-jar", "BiliLiveRecorder.jar", "debug=false&check=false&delete=false&liver=huya&id=" + roomId + "&retry=3&qn=-1&qnPri=钃濆厜4M>瓒呮竻>楂樻竻>娴佺晠"], { cwd: '/home/ubuntu' })
     proc.stdout.on("data", data => {
         output += data
     })
@@ -58,73 +25,35 @@ function startRecord(roomId) {
 
     proc.on("exit", () => {
         proc = null
-        //还在直播且不是因为主动终止，说明是因为异常退出的，要继续录制
-        if(isLive && !earlyTerminated){
-            startRecord(roomId)
+
+        if (!earlyTerminated) {
+            setTimeout(function () {
+                if (retryCount++ < 2)
+                    startRecord(roomId, retryCount)
+            }, 1000);
         }
+
     })
 }
 
-const danmuClient = new huya_danmu("lovetuleisi")
-//初始化弹幕模块，用来监听是否开播和下播
-danmuClient.on('message', msg => {
-    switch (msg.type) {
-        case 'beginLive':
-            //开始直播
-            console.log(JSON.stringify(msg))
-            output = "";
-            if (!isLive) {
-                isLive = true;
-                exec("jps", (error, stdout, stderr) => {
-                    if (error) {
-                        console.log(`error: ${error.message}`);
-                        return;
-                    }
-                    if (stderr) {
-                        console.log(`stderr: ${stderr}`);
-                        return;
-                    }
-                    if((/^.+\n$/.test(stdout))){
-                        startRecord("lovetuleisi")
-                    }
-                });
-            }
-            break
-        case 'endLive':
-            //结束直播
-            console.log(JSON.stringify(msg))
-            isLive = false;
-            break;
-    }
-})
-danmuClient.on('connect', () => {
-    console.log(`已连接huya lovetuleisi房间弹幕~`);
-})
-
-danmuClient.on('error', e => {
-    console.log(e)
-})
-
-danmuClient.on('close', () => {
-    console.log('开始断线重连')
-    danmuClient.start()
-})
-
-danmuClient.start()
-
+let set = new Set();
 app.get("/start", function (req, res) {
+
     if (proc != null) {
         res.send("Already started");
         return;
     }
+
     output = ""
+    retryCount = 0
     earlyTerminated = false
+
     let roomId = req.query.roomId;
-    startRecord(roomId == null || roomId == undefined ? "lovetuleisi" : roomId)
+    startRecord(roomId == null || roomId == undefined ? "lovetuleisi" : roomId, retryCount)
 
     res.send("OK,pid is " + proc.pid)
 })
-//查看磁盘用量
+
 app.get("/disk_usage", function (req, res) {
     exec("df -h", (error, stdout, stderr) => {
         if (error) {
@@ -169,6 +98,26 @@ mongoClient.connect(url, function (err, dbClient) {
     huyaDB = dbClient.db("huya");
 });
 
+/*
+var cors = require('cors')
+app.get("/getChatInfo", cors(), function (req, res) {
+    if (!huyaDB) {
+        res.send("Not ready yet")
+        return;
+    }
+    var startTime = new Date();
+    startTime.setDate(startTime.getDate() - 1)
+    startTime.setHours(19, 0, 0)
+    var queryParams = {
+        time: { $gt: startTime.getTime() },
+        type: 'chat'
+    }
+    huyaDB.collection("lovetuleisi").find(queryParams, { projection: { 'from.name': 1, 'type': 1, 'content': 1, 'time': 1 } }).sort({ time: 1 })
+        .toArray(function (err, result) {
+            res.send(result);
+        })
+})*/
+
 
 app.get("/getLatestChatInfos", function (req, res) {
     if (!huyaDB) {
@@ -182,6 +131,30 @@ app.get("/getLatestChatInfos", function (req, res) {
         })
 })
 
-var port = 8081
+app.get("/restartDanMuCrawler", function (req, res) {
+
+    var proc2 = spawn("sh", ["restart.sh"], { cwd: '/home/ubuntu/danmu-crawler' })
+    proc2.stdout.on("data", data => {
+        output += data
+    })
+
+    proc2.stderr.on("data", data => {
+        output += data
+    })
+
+    proc2.on('error', (error) => {
+        output = `error: ${error.message}`
+    })
+
+    res.send("ok")
+
+})
+
+app.get("/reset",function(req, res){
+    output = 0;
+    res.send("reset completed");
+})
+
+var port = 8082
 var server = app.listen(port)
 console.log('Server listened on: ' + port)
